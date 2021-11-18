d209b6e2dc3451f03c43edea7002e62572ebcce9
Durdiev9043 <donyorber9043@gmail.com>
2021-11-18 21:18:34 +0500

    trustelem

--- resources/views/trustelem.blade.php
+++ resources/views/trustelem.blade.php
@@ -25,29 +25,213 @@
             </div>
         </div>
     </section>
-    <section class="pack pack--white">
+    <section class="desc">
         <div class="container">
-            <div class="pack__title pack__title--p title">Попробуйте WALLIX Trustelem
-                бесплатно в течение месяца</div>
-            <div class="pack__desc">
-                WALLIX Trustelem предлагает мощное управление
-                правами конечных точек
+            <div class="desc__row">
+                <div class="desc__title">
+                    Унификация, безопасность и упрощение доступа пользователей
+                </div>
+                <div class="desc__text">
+                    Защита стратегических активов, повышение эффективности и производительности, а также улучшение пользовательского опыта как для пользователей приложений, так и для администраторов.<br><br>
+                    Упрощение и безопасность доступа к ключевым бизнес-приложениям с централизованной платформы.
+                </div>
             </div>
-            <div class="pack__row row">
-                <img src="{{asset('assets/img/pack-4.jpg')}}" />
-                <div class="pack__item">
-                    <div class="pack__text text">
+        </div>
+    </section>
+    <section class="security">
+        <div class="security__orange orange"></div>
+        <div class="container">
+            <div class="management__title">
+                Функции управления идентификацией
+            </div>
+            <div class="security__body row">
+                <img src="{{asset('assets/img/bastion-1.jpg')}}" />
+                <div class="security__desc">
+                    <div class="security__subtitle subtitle">Центральный каталог</div>
+                    <div class="security__text text">
+                        Соберите свои источники идентичности в единую ссылку на идентичность, чтобы легко управлять правами доступа в одном централизованном месте.
                         <ul>
-                            <li>Модель безопасности с наименьшими привилегиями</li>
-                            <li>Защита и предотвращение программ-вымогателей</li>
-                            <li>Контроль разрешений конечных точек</li>
-                            <li>Белый/серый/черный список приложений и процессов</li>
-                            <li>Блокировать привилегии уровня приложения/процесса</li>
-                            <li>Удобный пользовательский интерфейс для пользователей и администраторов</li>
-                            <li>Простая в использовании платформа управления приложениями</li>
+                            <li>Поддержка каталогов Active Directory, Azure AD, LDAP и Google G Suite directory</li>
+                            <li>Импортируйте столько каталогов сколько необходимо в вашу платформу WALLIX Trustelem</li>
+                            <li>Полный или частичный импорт каталога на основе членства в группе и фильтрации атрибутов</li>
+                            <li>Импорт членства в группах и атрибутов пользователей для определения надлежащих политик управления доступом</li>
                         </ul>
                     </div>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section class="news">
+        <div class="container">
+            <div class="news__row news__row--three">
+                <div class="news__item">
+                    <div class="news__subtitle">
+                        SSO
+                    </div>
+                    <div class="news__text">
+                        Предоставьте пользователям возможность единого входа...
+                    </div>
+                    <div class="news__xz">
+                        <img src="{{asset('assets/img/news-2.jpg')}}">
+                        <a href="#" class="news__link">
+                            Читать
+                        </a>
+                    </div>
+                </div>
+                <div class="news__item">
+                    <div class="news__subtitle w">
+                        Мид
+                    </div>
+                    <div class="news__text">
+                        Выберите один из множества механизмов многофакторной...
+                    </div>
+                    <div class="news__xz">
+                        <img src="{{asset('assets/img/news-3.jpg')}}">
+                        <a href="#" class="news__link">
+                            Читать
+                        </a>
+                    </div>
+                </div>
+                <div class="news__item">
+                    <div class="news__subtitle w">
+                        Самообслуживание
+                    </div>
+                    <div class="news__text">
+                        Предложите пользователям решение самообслуживания...
+                    </div>
+                    <div class="news__xz">
+                        <img src="{{asset('assets/img/news-3.jpg')}}">
+                        <a href="#" class="news__link">
+                            Читать
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="news__line line"></div>
+        </div>
+    </section>
+    <section class="zero">
+        <div class="container">
+            <div class="zero__row row">
+                <img src="{{asset('assets/img/zero-20.jpg')}}" />
+                <div class="zero__desc">
+                    <div class="zero__subtitle subtitle">
+                        Простая интеграция приложений
+                    </div>
+                    <div class="zero__text text">
+                        WALLIX Trustelem предварительно интегрировала широкий выбор бизнес-приложений, включая Office 365, G Suite, Salesforce, Box, Dropbox, ownCloud и NextCloud, Wordpress и многие другие, чтобы упростить реализацию Trustelem. Получите доступ к подробной документации для каждого приложения, чтобы упростить настройку.
+                    </div>
+                </div>
+            </div>
+            <div class="zero__row--resevre row">
+                <div class="zero__desc">
+                    <div class="zero__subtitle subtitle">
+                        Стройте быстро, бегайте легко
+                    </div>
+                    <div class="zero__text text">
+                        WALLIX Trustelem разработан для того, чтобы сделать администрирование простым и интуитивно понятным. Веб-консоль администрирования позволяет ИТ-персоналу легко настраивать синхронизацию каталогов, управлять жизненными циклами внешних учетных записей пользователей и приложений, создавать и адаптировать правила доступа, контролировать активность и проверять безопасность доступа.
+                    </div>
+                </div>
+                <img src="{{asset('assets/img/zero-21.jpg')}}" />
+            </div>
+            <div class="zero__row row">
+                <img src="{{asset('assets/img/zero-22.jpg')}}" />
+                <div class="zero__desc">
+                    <div class="zero__subtitle subtitle">
+                        Безопасность - ваша главная забота
+                    </div>
+                    <div class="zero__text text">
+                        Сохраняйте контроль над своими личностями и данными! Платформа WALLIX Trustelem размещается и управляется надежным европейским облачным провайдером. Суверенитет и безопасность ваших данных необходимы для обеспечения качества обслуживания и защиты активов вашей компании.
+                    </div>
+                </div>
+            </div>
+            <div class="zero__line line">
+            </div>
+        </div>
+    </section>
+    <section class="list">
+        <div class="container">
+            <div class="list__label title">Wallix Trustelem</div>
+            <div class="list__body">
+                <div class="list__item">
+                    <div class="list__icon">
+                        <div class="list__border">
+                            <img src="{{asset('assets/img/icon/list-6.svg')}}">
+                        </div>
+                    </div>
+                    <div class="list__desc">
+                        <div class="list__title">
+                            Обтекаемый
+                        </div>
+                        <div class="list__text">
+                            Trustelem предназначен для простоты пользовательского опыта и администрирования как на этапе сборки, так и на этапе запуска
+                        </div>
+                    </div>
+                </div>
+                <div class="list__item">
+                    <div class="list__icon">
+                        <div class="list__border">
+                            <img src="{{asset('assets/img/icon/list-7.svg')}}">
+                        </div>
+                    </div>
+                    <div class="list__desc">
+                        <div class="list__title">
+                            Безопасный
+                        </div>
+                        <div class="list__text">
+                            Безопасность лежит в основе инфраструктуры, программного обеспечения и операций Trustelem
+                        </div>
+                    </div>
+                </div>
+                <div class="list__item">
+                    <div class="list__icon">
+                        <div class="list__border">
+                            <img src="{{asset('assets/img/icon/list-8.svg')}}">
+                        </div>
+                    </div>
+                    <div class="list__desc">
+                        <div class="list__title">
+                            Европейский
+                        </div>
+                        <div class="list__text">
+                            WALLIX Trustelem принадлежит, разрабатывается, размещается и эксплуатируется в Европе, чтобы обеспечить нашу приверженность безопасности
+                        </div>
+                    </div>
+                </div>
+                <div class="list__item">
+                    <div class="list__icon">
+                        <div class="list__border">
+                            <img src="{{asset('assets/img/icon/list-9.svg')}}">
+                        </div>
+                    </div>
+                    <div class="list__desc">
+                        <div class="list__title">
+                            Экономически эффективный
+                        </div>
+                        <div class="list__text">
+                            Повышайте рентабельность инвестиций и экономию затрат благодаря привлекательной ценовой политике WALLIX Trustelem и надёжному предоставлению услуг
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="list__line line">
+            </div>
+        </div>
+    </section>
+    <section class="video">
+        <div class="video__bg" style="background: url('{{asset('assets/img/background/video-bg.jpg')}}');">
+        </div>
+        <div class="container">
+            <div class="video__title title">
+
+            </div>
+            <div class="video__desc">
+
+            </div>
+            <div class="video__block">
+                <video controls>
+                    <source src="{{asset('assets/video/2.mp4')}}" type="video/mp4">
+                </video>
             </div>
         </div>
     </section>
