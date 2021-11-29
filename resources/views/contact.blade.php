f328ccf19693fe5baf0a406ae5abeda8897a6ffd
anvar <anvarindastries@gmail.com>
2021-11-29 21:39:55 +0500

    Add map

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -68,7 +68,7 @@
                 </a>
             </div>
             <div class="map__body">
-                <img src="{{asset('assets/img/map.jpg')}}" alt="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d146261.87734742736!2d69.26927065813517!3d41.30040027364536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b0cc379e9c3%3A0xa5a9323b4aa5cb98!2z0KLQsNGI0LrQtdC90YIsINCj0LfQsdC10LrQuNGB0YLQsNC9!5e0!3m2!1sru!2s!4v1638203872908!5m2!1sru!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
     </section>
